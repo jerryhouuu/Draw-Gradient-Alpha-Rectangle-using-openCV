b37872a047a8ccc029b88a27d1fe796f1893f72a
Jerry <jerryhouuu@hotmail.com>
2020-06-03 15:29:14 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+def draw_gradient_alpha_rectangle(frame, BGR_Channel, rectangle_position, rotate):
+    (xMin, yMin), (xMax, yMax) = rectangle_position
+    color = np.array(BGR_Channel, np.uint8)[np.newaxis, :]
+    mask1 = np.rot90(np.repeat(np.tile(np.linspace(1, 0, (rectangle_position[1][1]-rectangle_position[0][1])), ((rectangle_position[1][0]-rectangle_position[0][0]), 1))[:, :, np.newaxis], 3, axis=2), rotate) 
+    frame[yMin:yMax, xMin:xMax, :] = mask1 * frame[yMin:yMax, xMin:xMax, :] + (1-mask1) * color
+
+    return frame
+
+def main():
+    frame = np.zeros((300, 300, 3), np.uint8)
+    frame[:,:,:] = 255
+
+    frame = draw_gradient_alpha_rectangle(frame, (42, 175, 121), ((0, 0), (300, 300)), 3)
+
+    cv2.imshow('frame', frame)
+    cv2.waitKey(0)
+
+if __name__ == '__main__':
+    main()
